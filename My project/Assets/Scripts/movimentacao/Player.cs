89a24635f445e391e1c1ba385f7b24ac9ca759e6
Luis Gabriel <137334103+Luisdo777@users.noreply.github.com>
2025-04-20 21:41:34 -0300

    guardiaodaamazonia1.5v
    adicionado o pulo unico

--- My project/Assets/Scripts/movimentacao/Player.cs
+++ My project/Assets/Scripts/movimentacao/Player.cs
@@ -4,19 +4,52 @@ using UnityEngine;
 
 public class Player :   MonoBehaviour
 {
+    private Rigidbody2D rbPlayer;
     public float speed;
+    private SpriteRenderer sr;
+    public float jumpForce;
+    public bool inFloor =  true;
     void Start()
     {
-        
+        sr = GetComponent<SpriteRenderer>();
+        rbPlayer = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        MovePlayer();
     }
 
     void Update()
     {
-        MovePlayer();
+        Jump();
     }
     void MovePlayer()
     {
-        float horizontalMoviment = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(horizontalMoviment * Time.deltaTime * speed,0,0);
+        float horizontalMoviment = Input.GetAxisRaw("Horizontal");
+        rbPlayer.linearVelocity = new Vector2(horizontalMoviment * speed, rbPlayer.linearVelocity.y);
+
+        if (horizontalMoviment > 0)
+        {
+            sr.flipX = false;
+        }
+
+        else if (horizontalMoviment < 0)
+        {
+            sr.flipX = false;
+        }
+    }
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && inFloor)
+        {
+            rbPlayer.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            inFloor = false;
+        }
+    }
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.name == "ground"){
+            inFloor = true;
+        }
     }
 }
